598da99abe24779c8e2a6001b137ece8e132fb2f
Alysson <alyssonoliveira456@gmail.com>
2023-10-12 03:00:39 -0300

    Separando em arquivo

--- Produtos/Remedios.java
+++ Produtos/Remedios.java
@@ -6,7 +6,6 @@ public abstract class Remedios extends Produtos implements Tarja, Desconto {
     public Remedios(String nome, double preco, int estoque, String dataValidade, String fabricante) {
         super(nome, preco, estoque, dataValidade, fabricante);
     }
-
     @Override
     public void setDesconto() {
     }
